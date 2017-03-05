e3b4eb04c0829c75f94cb364cf237c57a6c54806
Sebastien Schaeffler <bidisoft@gmail.com>
2017-03-05 18:09:01 +0100

    Add widgets to empty dashboard

--- src/modules/dashboard/components/DashboardPage.js
+++ src/modules/dashboard/components/DashboardPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {injectIntl} from 'react-intl';
+import {FormattedDate,injectIntl} from 'react-intl';
+import Clock from 'react-clock';
+import FontAwesome from 'react-fontawesome';
 import PageHeader from '../../../components/library/PageHeader';
 import msg, {Keys} from './DashboardPage_messages';
 import appMsg, {Keys as AppKeys} from '../../../i18n/keys';
@@ -16,8 +18,30 @@ class DashboardPage extends Component {
                     rootText={this.props.intl.formatMessage(appMsg(AppKeys.APP_TITLE))}/>
         <div className="spacer"/>
         <div className="row" style={{ marginLeft: 6 }}>
-          <div className='col-lg-4 col-md-2'>
-            No indicators are available yet.
+          <div className="row">
+            <div className="col-lg-2 col-md-6">
+              <div className="panel panel-info panel-alt widget-today">
+                <div className="panel-heading text-center">
+                  <FontAwesome name="calendar" />
+                  {/*<div className="calendar-div"><span className="calendar-text">3 events</span></div>*/}
+                </div>
+                <div className="panel-body text-center">
+                  <h3 className="today">
+                    <FormattedDate value={Date.now()} day='2-digit' month='short' year='numeric' />
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-6">
+              <div className="panel panel-info panel-alt widget-time">
+                <div className="panel-heading text-center">
+                  <FontAwesome name="clock-o" />
+                </div>
+                <div className="panel-body text-center">
+                  <h3 className="today"><Clock /></h3>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="spacer"/>
